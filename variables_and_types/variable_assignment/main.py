05f3b2b852cd423c1f6662dcb6962731b43030ed
woodi1983 <>
2026-03-08 09:35:29 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,10 @@
+item_name = "Eggs"              # string needs quotes
+item_price = 3.99               # this was fine
+item_stock_quantity = 120       # this was fine
+in_stock = True                 # capitalize boolean
+
+# Testing
+print("Item : ", item_name)
+print("Price per unit: $", item_price)
+print("quantity in stock: ", item_stock_quantity)
+print("In stock: ", in_stock)
