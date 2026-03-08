3c1a8f7bca3641cf4698b50b440d7e1af313fd91
woodi1983 <>
2026-03-08 10:58:42 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,9 @@
+#我左边的是我自定的自变量，等式右边的是它的数值（或者结果）
+item_name ="Bread"
+item_price = 4.52
+items_in_stock = 230
+# print括号里面的带“”的是输出的结果的显示，而逗号后面的则是自编量，有点像F（x）
+# Testing
+print("Item:", item_name)
+print("Price per loaf: $", item_price)
+print("Quantity in stock:", items_in_stock)
